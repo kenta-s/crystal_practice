49c41dd0549799e6c3677dccb8f215461e7284d0
kenta-s <knt01222@gmail.com>
2017-08-09 13:47:53 +0900

    Fix spec(just a work around)

--- HelloWorld/spec/HelloWorld_spec.cr
+++ HelloWorld/spec/HelloWorld_spec.cr
@@ -1,9 +1,8 @@
 require "./spec_helper"
 
 describe HelloWorld do
-  # TODO: Write tests
-
   it "works" do
-    false.should eq(true)
+    str = "Hello World"
+    str.should eq("Hello World")
   end
 end
