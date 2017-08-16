dbd6d20a483559184d1946c16f221f3b9c309cd7
kenta-s <knt01222@gmail.com>
2017-08-16 14:15:47 +0900

    Twitter OAuth

--- CallingTwitterApi/src/CallingTwitterApi.cr
+++ CallingTwitterApi/src/CallingTwitterApi.cr
@@ -1,5 +1,16 @@
 require "./CallingTwitterApi/*"
+require "http/client"
+require "oauth"
 
 module CallingTwitterApi
-  # TODO Put your code here
+  token = ENV["TWITTER_ACCESS_TOKEN"]
+  secret = ENV["TWITTER_TOKEN_SECRET"]
+  consumer_key = ENV["TWITTER_KEY"]
+  consumer_secret = ENV["TWITTER_SECRET"]
+
+  client = HTTP::Client.new("api.twitter.com", tls: true)
+  OAuth.authenticate(client, token, secret, consumer_key, consumer_secret)
+
+  response = client.get("/search")
+  puts response.status_code
 end
