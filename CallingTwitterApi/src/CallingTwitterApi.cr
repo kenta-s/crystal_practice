3ba5d35bd73a5abca161428f9e4a1f2502f45d43
kenta-s <knt01222@gmail.com>
2017-08-16 15:58:56 +0900

    OAuth and post tweet

--- CallingTwitterApi/src/CallingTwitterApi.cr
+++ CallingTwitterApi/src/CallingTwitterApi.cr
@@ -1,16 +1,19 @@
 require "./CallingTwitterApi/*"
 require "http/client"
 require "oauth"
+require "uri"
 
-module CallingTwitterApi
-  token = ENV["TWITTER_ACCESS_TOKEN"]
-  secret = ENV["TWITTER_TOKEN_SECRET"]
-  consumer_key = ENV["TWITTER_KEY"]
-  consumer_secret = ENV["TWITTER_SECRET"]
+token = ENV["TWITTER_ACCESS_TOKEN"]
+secret = ENV["TWITTER_TOKEN_SECRET"]
+consumer_key = ENV["TWITTER_KEY"]
+consumer_secret = ENV["TWITTER_SECRET"]
 
-  client = HTTP::Client.new("api.twitter.com", tls: true)
-  OAuth.authenticate(client, token, secret, consumer_key, consumer_secret)
+client = HTTP::Client.new("api.twitter.com", tls: true)
+OAuth.authenticate(client, token, secret, consumer_key, consumer_secret)
 
-  response = client.get("/search")
-  puts response.status_code
-end
+response = client.get("/1.1/users/search.json?q=foo")
+puts response.status_code
+puts response.body
+
+response = client.post_form("/1.1/statuses/update.json", { status: "日本語ツイートのテスト" })
+puts response.body
